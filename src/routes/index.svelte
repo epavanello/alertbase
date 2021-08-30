ba0892b1b3d485de94fbdc99522a8b251ff50d60
Emanuele Pavanello <pavanello.emanuele@gmail.com>
2021-08-30 22:05:44 +0200

    Svelte to sveltekit

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Tailwind from '$lib/Tailwind.svelte'
+</script>
+
+<Tailwind />
+<main class="container mx-auto">
+	<atricle class="prose">
+		<h1 class="text-center">Welcome to Alertbase</h1>
+	</atricle>
+</main>
