2f9484ccf30a30172253cbb9d826827e6e3e479c
Emanuele Pavanello <pavanello.emanuele@gmail.com>
2021-08-30 20:44:42 +0200

    Project setup: tailwindcss, eslint, prettier

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./public/index.html', './src/**/*.{svelte,js,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
