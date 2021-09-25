c93213f05d6835d058d893643ff142a037a0d242
Emanuele Pavanello <pavanello.emanuele@gmail.com>
2021-09-25 19:02:15 +0200

    Header

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import Footer from '$components/Footer.svelte'
+  import Header from '$components/Header.svelte'
   import Tailwind from '$styles/Tailwind.svelte'
 </script>
 
 <Tailwind />
+<Header />
 <main class="container mx-auto h-full">
   <atricle class="prose">
     <h1 class="text-center dark:text-white py-4">Welcome to Alertbase</h1>
