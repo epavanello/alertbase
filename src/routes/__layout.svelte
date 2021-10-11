88d536b356981690c87d4bcd2f3f956f4a4175b8
Emanuele Pavanello <pavanello.emanuele@gmail.com>
2021-10-11 23:12:09 +0200

    debug

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,13 +9,8 @@
 
   let urlSearch = new URLSearchParams(window.location.hash.substr(1))
 
-  console.log(
-    urlSearch.toString(),
-    urlSearch.has('type'),
-    urlSearch.has('access_token'),
-    urlSearch.get('access_token'),
-    urlSearch.get('type') == 'recovery'
-  )
+  console.log('access_token', urlSearch.get('access_token'))
+  console.log('type', urlSearch.get('type'))
 
   if (urlSearch.has('type') && urlSearch.has('access_token') && urlSearch.get('type') == 'recovery') {
     goto(`/resetPassword#access_token=${urlSearch.get('access_token')}`)
